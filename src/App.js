633b4f743c591f9d7e8e43b299548c2966ef2c8d
Austin Gregersen <abgregs@gmail.com>
2022-02-09 15:07:53 -0700

    add a simple header and title for our app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main ui text container">
+        <header>
+          <h1 className="ui dividing centered header">Technologies I Use</h1>
+        </header>
+        <div id="content"></div>
+      </div>
     </div>
   );
 }
