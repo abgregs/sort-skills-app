5e918c7c91b5f95f427d7a8fade8eac1954c8039
Austin Gregersen <abgregs@gmail.com>
2022-02-09 15:50:17 -0700

    create some data with a few initial items to go in our list

---
+++ src/items.js
@@ -0,0 +1,38 @@
+window.Items = (function () {
+  function generateVoteCount() {
+    return Math.floor((Math.random() * 50) + 15);
+  }
+
+const items = [
+    {
+      id: 1,
+      title: 'Sass',
+      description: 'Sass is the most mature, stable, and powerful professional grade CSS extension language in the world.',
+      url: 'https://sass-lang.com/',
+      votes: generateVoteCount(),
+    },
+    {
+      id: 2,
+      title: 'Tailwind CSS',
+      description: 'A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.',
+      url: 'https://tailwindcss.com/',
+      votes: generateVoteCount(),
+    },
+    {
+      id: 3,
+      title: 'WordPress',
+      description: 'The world&#39;s most popular website builder',
+      url: 'https://wordpress.com/',
+      votes: generateVoteCount(),
+    },
+    {
+      id: 4,
+      title: 'React.js',
+      description: 'A JavaScript library for building user interfaces',
+      url: 'https://reactjs.org/',
+      votes: generateVoteCount(),
+    },
+  ];
+
+  return { items: items };
+}());
