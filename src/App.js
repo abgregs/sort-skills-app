6907099dd20837530050f6daed7f9a31ab4130d9
Austin Gregersen <abgregs@gmail.com>
2022-02-09 15:56:02 -0700

    create and display a simple react component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  class ItemList extends React.Component {
+    render() {
+      return (
+        <div className='ui unstackable items'>
+          Hello, friend! I am a basic React component.
+        </div>
+      );
+    }
+  }
+
   return (
     <div className="App">
       <div className="main ui text container">
         <header>
           <h1 className="ui dividing centered header">Technologies I Use</h1>
         </header>
-        <div id="content"></div>
+        <div id="content">
+          <ItemList />
+        </div>
       </div>
     </div>
   );
