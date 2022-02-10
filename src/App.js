2989cc9debf8ca8507ef61a563934696a6a204fd
Austin Gregersen <abgregs@gmail.com>
2022-02-10 15:55:17 -0700

    add icons to our data set and render them in our list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
 import data from './data.js'
 
+import * as DiIcons from 'react-icons/di';
+import * as SiIcons from 'react-icons/si';
+
 function App() {
+
+  const IconMissing = (props) => {
+    return (
+      <div className="icon-missing">{props.title}</div>
+    )
+  }
+
+  const DynamicDiIcon = (props) => {
+    const IconComponent = DiIcons[props.icon];
+
+    if (!IconComponent) { // Return a default one
+      return <IconMissing title={props.title}/>;
+    }
+
+    return <IconComponent title={props.title}/>;
+  };
+
+  const DynamicSiIcon = (props) => {
+    const IconComponent = SiIcons[props.icon];
+
+    if (!IconComponent) { // Return a default one
+      return <IconMissing title={props.title}/>;
+    }
+
+    return <IconComponent />;
+  };
 
   class ItemList extends React.Component {
     render() {
@@ -13,6 +43,8 @@ function App() {
           description={item.description}
           url={item.url}
           votes={item.votes}
+          icon={item.icon}
+          iconLibrary = {item.iconLibrary}
         />
       ));
       return (
@@ -24,11 +56,18 @@ function App() {
   }
 
   class Item extends React.Component {
+
     render() {
       return (
         <div className='item'>
           <div className='middle aligned content'>
             <div className='header'>
+              <div className='extra'>
+                <a href={this.props.url} target="_blank">
+                  {this.props.iconLibrary === 'di' && <DynamicDiIcon icon={this.props.icon} title={this.props.title}></DynamicDiIcon>}
+                  {this.props.iconLibrary === 'si' && <DynamicSiIcon icon={this.props.icon} title={this.props.title}></DynamicSiIcon>}
+                </a>
+              </div>
               <a>
                 Vote {/* TODO: Vote button will go here */}
               </a>
--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
-  function generateVoteCount() {
-    return Math.floor((Math.random() * 50) + 15);
-  }
+function generateVoteCount() {
+  return Math.floor((Math.random() * 50) + 15);
+}
 
   const items = [
     {
@@ -9,6 +9,8 @@
       description: 'Sass is the most mature, stable, and powerful professional grade CSS extension language in the world.',
       url: 'https://sass-lang.com/',
       votes: generateVoteCount(),
+      icon: 'DiSass',
+      iconLibrary: 'di',
     },
     {
       id: 2,
@@ -16,20 +18,26 @@
       description: 'A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.',
       url: 'https://tailwindcss.com/',
       votes: generateVoteCount(),
+      icon: 'SiTailwindcss',
+      iconLibrary: 'si',
     },
     {
       id: 3,
       title: 'WordPress',
-      description: 'The world\'s most popular website builder',
+      description: 'The world\'s most popular website builder.',
       url: 'https://wordpress.com/',
       votes: generateVoteCount(),
+      icon: 'DiWordpress',
+      iconLibrary: 'di',
     },
     {
       id: 4,
       title: 'React.js',
-      description: 'A JavaScript library for building user interfaces',
+      description: 'A JavaScript library for building user interfaces.',
       url: 'https://reactjs.org/',
       votes: generateVoteCount(),
+      icon: 'DiReact',
+      iconLibrary: 'di',
     },
   ];
 
